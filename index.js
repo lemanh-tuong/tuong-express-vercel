0533e8571aea92411896f4e7f8b0f93ce93fcd4a
lemanh-tuong <letuong05092000@gmail.com>
2022-12-03 10:27:06 +0700

    1st commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const { v4 } = require("uuid");
+const app = express();
+const port = 49054;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.get("/posts", (req, res) => {
+	const posts = JSON.parse(fs.readFileSync("./posts.json", "utf8"));
+	res.json(posts);
+});
+
+app.post("/posts", (req, res) => {
+	const data = { id: v4(), ...req.body };
+	const posts = JSON.parse(fs.readFileSync("./posts.json", "utf8")).concat(data);
+	fs.writeFileSync("./posts.json", JSON.stringify(posts, undefined, 2));
+	res.json(data);
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
